116725f10b047f0b0e196bf668bd383c1ab0f33f
Sairam <sairamtsramesh@gmail.com>
2025-07-31 08:59:18 +0530

    Initial commit: Angular project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookFormComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
